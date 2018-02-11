aaaaa8cb4d9be89ef97029a882e05c34c10823b5
Will Price <will.price94@gmail.com>
2018-02-11 18:41:19 +0000

    Move examples into system tests folder

--- tests/system/test_all_checkers.sh
+++ tests/system/test_all_checkers.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bats
 
-SCRIPT_PATH="$BATS_TEST_DIRNAME"
-export ROOT="$SCRIPT_PATH/../.."
+export ROOT="$BATS_TEST_DIRNAME/../.."
+export EXAMPLES_ROOT="$BATS_TEST_DIRNAME/examples"
 export PYTHONPATH="$ROOT"
 
 cd "$ROOT"
@@ -18,13 +18,13 @@ run_pylint() {
 }
 
 @test "Method too long rule" {
-    run_pylint "$ROOT/examples/method_length.py" method-too-long
+    run_pylint "$EXAMPLES_ROOT/method_length.py" method-too-long
 }
 
 @test "Cyclomatic complexity too high" {
-    run_pylint "$ROOT/examples/cyclomatic_complexity.py" too-complex
+    run_pylint "$EXAMPLES_ROOT/cyclomatic_complexity.py" too-complex
 }
 
 @test "Too many methods rule" {
-    run_pylint "$ROOT/examples/method_count_in_class.py" too-many-methods
+    run_pylint "$EXAMPLES_ROOT/method_count_in_class.py" too-many-methods
 }
